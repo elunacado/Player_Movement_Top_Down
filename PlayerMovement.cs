9ff725d46cda7218989bfc246d37b68e07d9d36c
elunacado <54047744+elunacado@users.noreply.github.com>
2021-05-26 13:20:23 -0500

    Add files via upload

---
+++ PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float MoveSpeed = 5f;
+    public Rigidbody2D rb;
+
+    Vector2 movement;
+   
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+      
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * MoveSpeed * Time.fixedDeltaTime);
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.transform.tag=="danger")
+        {
+            gameObject.GetComponent<Animator>().SetBool("Loud", true);
+        }
+        
+    }
+}
